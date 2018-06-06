0caee18a7b1353c3cf1e9c9c63d6ccbe89b51ac0
ElectronicManuel <sedlaceke@bzz.ch>
2018-06-06 17:04:19 +0200

    changing logged in to typescript

---
+++ src/vsync.d.ts
@@ -0,0 +1,19 @@
+declare namespace vsync {
+    interface Profile {
+        key: string,
+        name: string,
+        startTime: number,
+        endTime: number,
+        currentTime: number,
+        urlPattern: string,
+        currentURL?: string,
+        videoHost?: string,
+        videoQuery?: string,
+        nextHost?: string,
+        nextQuery?: string,
+        latestFrame?: string
+    }
+
+    class Profile implements Profile {
+    }
+}
