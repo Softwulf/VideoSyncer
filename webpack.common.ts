42ab8e4c442c3606bbb67e469a085fffbfc7834a
ElectronicManuel <sedlaceke@bzz.ch>
2018-08-12 03:41:58 +0200

    adding base webpack config

---
+++ webpack.common.ts
@@ -0,0 +1,104 @@
+import * as path from 'path';
+import * as webpack from 'webpack';
+import * as glob from 'glob';
+import * as MergeJsonWebpackPlugin from 'merge-jsons-webpack-plugin';
+import * as UglifyJsPlugin from 'uglifyjs-webpack-plugin';
+import * as CopyWebpackPlugin from 'copy-webpack-plugin';
+import * as CleanWebpackPlugin from 'clean-webpack-plugin'
+
+const calculateEntryObject = () => {
+    const fileEndings = [
+        '.entry.jsx',
+        '.entry.tsx',
+        '.entry.js',
+        '.entry.ts'
+    ]
+
+    const entryArray = glob.sync('./src/**/*.entry.*');
+    const entryMap = {};
+    entryArray.forEach(entryPath => {
+        let sanitizedName = entryPath.replace('./src/', '');
+        fileEndings.forEach(ending => {
+            sanitizedName = sanitizedName.replace(ending, '.js');
+        });
+        entryMap[sanitizedName] = entryPath;
+    });
+
+    return entryMap;
+}
+
+const generateConfig = (env) : webpack.Configuration => {
+    const prod = env
+        ? env.prod
+        : false;
+    const version = env
+        ? env.version
+        : '0.0.0';
+
+    const entry = calculateEntryObject();
+
+    const distDir = 'dist/base';
+
+    const plugins = [
+        new CopyWebpackPlugin([
+            {
+                from: '**/*',
+                ignore: [
+                    '**/*.tsx',
+                    '**/*.ts',
+                    '**/*.jsx',
+                    '**/*.js'
+                ],
+                context: `${__dirname}/src/`
+            }
+        ], {
+            copyUnmodified: true
+        }),
+        new CleanWebpackPlugin([distDir], {
+            watch: true
+        })
+    ];
+
+    if (prod) {
+        plugins.push(
+            new UglifyJsPlugin({
+                parallel: true
+            })
+        );
+    }
+
+    const config : webpack.Configuration = {
+        mode: prod
+            ? 'production'
+            : 'development',
+        entry,
+        devtool: 'source-map',
+        output: {
+            path: path.resolve(__dirname, distDir),
+            filename: '[name]'
+        },
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js', '.jsx']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|tsx|js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: 'ts-loader'
+                }, {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader']
+                }, {
+                    test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                    use: 'url-loader'
+                }
+            ]
+        },
+        plugins
+    }
+
+    return config;
+};
+
+export default generateConfig;
