0fffcd1a67cd5b18e229a85e80fd21ac8137e2cb
ElectronicManuel <sedlaceke@bzz.ch>
2018-08-14 01:38:17 +0200

    adding script to change body dimensions on android

--- src/pages/browser-action/browser-action.entry.tsx
+++ src/pages/browser-action/browser-action.entry.tsx
@@ -8,12 +8,31 @@ import { ThemeProvider } from 'components/theme-provider';
 import { AuthProvider } from 'components/auth-provider';
 import { MainLayout } from './main-layout';
 import { ReduxProvider } from '../_redux/redux-provider';
+import { browser } from 'webextension-polyfill-ts';
 
 
 
 class BrowserAction extends React.Component<{}, {}> {
     constructor(props) {
         super(props);
+    }
+
+    async componentDidMount() {
+        // change html & body height / width because on android the popup runs in a tab
+
+        let changeBodyDimensions = false;
+        if(browser.runtime && browser.runtime.getPlatformInfo) {
+            const platformInfo = await browser.runtime.getPlatformInfo();
+            if(platformInfo.os === 'android') changeBodyDimensions = true;
+        }
+        
+        if(changeBodyDimensions) {
+            window.document.getElementsByTagName('html').item(0).style.width = '100%';
+            window.document.getElementsByTagName('html').item(0).style.height = '100%';
+
+            window.document.getElementsByTagName('body').item(0).style.width = '100%';
+            window.document.getElementsByTagName('body').item(0).style.height = '100%';
+        }
     }
 
     render() {
